b31ad1eea003046e9ef539094506a7de5d01fdf9
Basma Eldesoukii <41978905+basmaEldesoukii@users.noreply.github>
2024-12-30 16:57:52 +0200

    feat: completing main activity

--- app/src/main/java/com/basma/foodorderingapp/MainActivity.kt
+++ app/src/main/java/com/basma/foodorderingapp/MainActivity.kt
@@ -1,47 +1,28 @@
 package com.basma.foodorderingapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.appcompat.app.AppCompatActivity
 import com.basma.foodorderingapp.ui.theme.FoodOrderingAppTheme
+import com.basma.presentation.component.AppNavigation
+import com.basma.presentation.component.ForcePortraitOrientation
+import com.basma.presentation.viewmodel.TablesViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private val tablesViewModel: TablesViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         enableEdgeToEdge()
         setContent {
             FoodOrderingAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Lock orientation to portrait mode
+                ForcePortraitOrientation()
+                AppNavigation(tablesViewModel)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FoodOrderingAppTheme {
-        Greeting("Android")
-    }
-}
